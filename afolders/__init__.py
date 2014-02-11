b54c71c8e03436a3e5ea94e3a01bb61d695549c9
alex <alex.valiushko@cybera.ca>
2014-02-10 22:57:34 -0700

    basic daemon class

---
+++ afolders/__init__.py
@@ -0,0 +1,11 @@
+from afolders.main import EmptyDaemon
+import logging
+
+logging.basicConfig(
+    filename='active-folders.log',
+    level=logging.DEBUG,
+    format="%(asctime)-15s - %(levelname)s::%(name)s - %(message)s")
+
+LOG = logging.getLogger(__name__)
+
+default_dtn = EmptyDaemon(pidfile='/tmp/active-folders.pid')
---
+++ afolders/main.py
@@ -0,0 +1,15 @@
+from afolders.utils import Daemon
+import logging
+import time
+
+LOG = logging.getLogger(__name__)
+
+
+class EmptyDaemon(Daemon):
+    """
+    Empty background process
+    """
+    def run(self):
+        LOG.info("Empty daemon is up")
+        while True:
+            time.sleep(1)
