4a37ce269c49f32359015c649e28daa23d884ef7
Cameron Mann <camthemann@gmail.com>
2014-02-18 16:06:35 -0700

    Added example file monitoring code

---
+++ activefolders/monitor.py
@@ -0,0 +1,21 @@
+import sys
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+class DtnHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        print(event)
+
+if __name__ == "__main__":
+    path = sys.argv[1] if len(sys.argv) > 1 else '.'
+    event_handler = DtnHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
