a224f23570f99dfa3db15835bb7a77df26eedc7f
alex <alex.valiushko@cybera.ca>
2014-02-21 14:39:59 -0700

    move daemon instantiation to runner

--- activefolders/__init__.py
+++ activefolders/__init__.py
@@ -1,4 +1,3 @@
-from activefolders.main import DtnDaemon
 import logging
 
 logging.basicConfig(
@@ -6,5 +5,3 @@ logging.basicConfig(
     level=logging.DEBUG,
     format="%(asctime)-15s - %(levelname)s::%(name)s - %(message)s")
 LOG = logging.getLogger(__name__)
-
-default_dtn = DtnDaemon(pidfile='/tmp/dtn-daemon.pid')
