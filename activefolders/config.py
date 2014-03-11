9d4b106145d3c048c8220791e40687cfad30443a
Cameron Mann <camthemann@gmail.com>
2014-03-11 15:25:06 -0600

    Remove old file

--- activefolders/config.py
+++
@@ -1,8 +0,0 @@
-import configparser
-import os
-
-activefolders = configparser.ConfigParser()
-activefolders.read(['/etc/activefolders.conf', os.path.expanduser('~/.activefolders.conf')])
-
-destinations = configparser.ConfigParser()
-destinations.read(['/etc/activefolders/destinations.conf'])
