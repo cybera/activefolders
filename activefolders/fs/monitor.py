c6870afe873ce1b9eb2c15678dd47386219bbfb2
Cameron Mann <camthemann@gmail.com>
2014-03-27 15:01:15 -0600

    More logging

--- activefolders/fs/monitor.py
+++ activefolders/fs/monitor.py
@@ -1,11 +1,14 @@
 from watchdog.observers.polling import PollingObserver
 from activefolders.fs.seafile import SeafilePollingHandler
+import logging
 import activefolders.conf as conf
 
+LOG = logging.getLogger(__name__)
 observer = None
 
 
 def start():
+    LOG.info("starting filesystem monitor")
     global observer
     if observer is None:
         path = conf.settings['dtnd']['storage_path']
@@ -16,5 +19,6 @@ def start():
 
 
 def stop():
+    LOG.info("stopping filesystem monitor")
     observer.stop()
     observer.join()
