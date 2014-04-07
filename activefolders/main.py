653c30382c95e9d645800f539f8c5577ecafe1bb
alex <alex.valiushko@cybera.ca>
2014-04-07 14:42:56 -0600

    switch to native daemonization procedure

--- activefolders/main.py
+++ activefolders/main.py
@@ -1,4 +1,3 @@
-from activefolders.utils import Daemon
 import logging
 import activefolders.api as api
 import activefolders.db as db
@@ -8,18 +7,9 @@ import activefolders.controllers.transfers as transfers
 LOG = logging.getLogger(__name__)
 
 
-class DtnDaemon(Daemon):
-    """
-    DTN background process
-    """
-    def run(self):
-        LOG.info("DTN daemon is starting")
-        db.init()
-        folders.check()
-        transfers.check()
-        api.start()
-
-
 def default_start():
-    daemon = DtnDaemon(pidfile="/var/run/dtnd.pid")
-    daemon.start()
+    LOG.info("DTN daemon is starting")
+    db.init()
+    folders.check()
+    transfers.check()
+    api.start()
