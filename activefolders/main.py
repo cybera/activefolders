7f3dca7d4eda7bf1d66e1dd4fd23ff31b10520d7
Cameron Mann <camthemann@gmail.com>
2014-07-28 16:12:19 -0600

    Start the monitoring thread

--- activefolders/main.py
+++ activefolders/main.py
@@ -1,6 +1,7 @@
 from activefolders.api import app
-import activefolders.conf as conf
 from activefolders.utils import LOG
+from activefolders.monitor import Monitor
+import activefolders.conf as conf
 
 
 def start():
@@ -8,4 +9,5 @@ def start():
     port = conf.settings['dtnd']['listen_port']
 
     LOG.info("DTN daemon is starting on {}:{}".format(host, conf))
+    Monitor().start()
     app.run(host=host, port=port, debug=True)
